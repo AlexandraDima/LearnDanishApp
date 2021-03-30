3c3389298bf684611cd0d59994c68bd2193cc6c4
Alexandra Dima <alexandra.dima@bestseller.com>
2021-03-30 14:51:48 +0200

    Adding menu navigation

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Default msg="Welcome to Your Vue.js App" class="text-light"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Default from '@/components/Default.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Default
+  }
+}
+</script>
