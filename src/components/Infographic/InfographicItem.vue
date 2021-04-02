af58fd8d4713a1e995ad1eb9f2c47f4c31ee8aa2
Alexandra Dima <alexandra.dima89@yahoo.com>
2021-04-02 11:16:56 +0200

    Adding masonry grid to infographics container

--- src/components/Infographic/InfographicItem.vue
+++ src/components/Infographic/InfographicItem.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div class="infographicItem_container">
     <img :src="require(`@/assets/Infographics/${imagePath}`)" />
     <p>{{imageName}}</p>
   </div>
+  <!--  <b-col lg="4" class="infographicItem_container">
+    <img :src="require(`@/assets/Infographics/${imagePath}`)" />
+    <p>{{imageName}}</p>
+  </b-col> -->
 </template>
 
 <script>
@@ -16,6 +20,21 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+.infographicItem_container {
+    display: inline-block;
+    background: #fff;
+    padding: 0.5em;
+    margin: 0 0 1.5em;
+    width: 100%;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-shadow: 2px 2px 4px 0 #ccc;
+
+    img {
+      max-width:100%; height: auto;
+      }
+}
 
 </style>
--- src/views/Infographics.vue
+++ src/views/Infographics.vue
@@ -1,13 +1,18 @@
 <template>
-  <b-row class="pt-4">
-      <b-col lg="4">
-        <InfographicItem v-for="(item, index) in infographics" 
+<!--   <b-row class="infographics__container pt-4">
+        <InfographicItem ref="infographicItem" v-for="(item, index) in infographics" 
             :key="index" 
             :imagePath="item.url"
             :imageName="item.name"
         />
-        </b-col>
-  </b-row>
+  </b-row> -->
+    <div class="infographics_container">
+        <InfographicItem ref="infographicItem" v-for="(item, index) in infographics" 
+                    :key="index" 
+                    :imagePath="item.url"
+                    :imageName="item.name"
+                />
+    </div>
 </template>
 
 <script>
@@ -22,7 +27,7 @@ export default {
         return {
             infographics : [
                 {
-                    url : 'Adverbs.svg',
+                    url : 'Adverbs2.svg',
                     name : 'Adverbs',
                 },
                 {
@@ -30,24 +35,63 @@ export default {
                     name : 'Body parths'
                 },
                 {
-                    url : 'FixedExpression.svg',
+                    url : 'FixedExpression2.svg',
                     name : 'Fixed expressions'
                 },
                 {
-                    url : 'Inversion.svg',
+                    url : 'Inversion2.svg',
                     name : 'Inversion'
                 },
                 {
-                    url : 'Numbers.svg',
+                    url : 'Numbers2.svg',
                     name : 'Numbers'
-                }
+                },
+                {
+                    url : 'Adverbs2.svg',
+                    name : 'Adverbs',
+                },
+                {
+                    url : 'BodyHealth.svg',
+                    name : 'Body parths'
+                },
+                {
+                    url : 'Adverbs2.svg',
+                    name : 'Adverbs',
+                },
+                  {
+                    url : 'Numbers2.svg',
+                    name : 'Numbers'
+                },
             ]
         }
-    }
+    },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.infographics_container {
+   -webkit-column-count: 1;
+    -moz-column-count:1;
+    column-count: 1;
+    -webkit-column-gap: 1rem;
+    -moz-column-gap: 1rem;
+    column-gap: 1rem;
+    padding: 0;
+    margin: 1rem;
+}
+@media only screen and (min-width: 900px){
+    .infographics_container {
+        -moz-column-count: 2;
+        -webkit-column-count: 2;
+        column-count: 2;
+    }
+}
+@media only screen and (min-width: 1200px){
+    .infographics_container {
+        -moz-column-count: 3;
+        -webkit-column-count: 3;
+        column-count: 3;
+    }
+}
 </style>
